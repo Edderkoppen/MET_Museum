309de9e25b7352ea04306c1aaf19f3ea9f778da6
Victor <yangv140@gmail.com>
2023-04-24 18:37:45 +0200

    Change navbar and footer
    almost last version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,14 @@ export default function App() {
     <footer className="bg text-center">
         <div className="container p-4 pb-0">
             <section className="">
-            <form action="">
-                <div className="row d-flex justify-content-center">
+            <form className="align" action="">
+                <div className="">
                 <div className="col-auto">
                     <p className="pt-2">
-                    <strong>Sign up for our newsletter</strong>
+                    <strong className="color">Sign up for our newsletter</strong>
                     </p>
                 </div>
-                <div className="col-md-5 col-12">
+                <div className="center">
                     <div className="form-outline mb-4">
                         <input type="email" id="form5Example27" className="form-control" placeholder="Email"/>
                     </div>
@@ -29,7 +29,7 @@ export default function App() {
 
         <div className="text-center p-3" >
             © 2020 Copyright:
-            <a className="text-dark deco" href="https://mdbootstrap.com/"> Met.com</a>
+            <a className="text-white deco" href="https://mdbootstrap.com/"> Met.com</a>
         </div>
     </footer>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,14 +5,11 @@ import "../compoments.scss/NavBar.scss"
 export default function NavBar() {
   return (
     <nav className="nav-link-from">
-        <h1><a className="nav-link-left" href="#">Navbar</a></h1>
+        <a className="nav-link-left" href="#"><img className="nav-link-img" src="https://centaur-wp.s3.eu-central-1.amazonaws.com/designweek/prod/content/uploads/2016/02/20100425/WO_Met_Wired_21816_2.jpg" alt=""/><h1>Documentation</h1></a>
         <form className="nav-from-right">
             <input className="nav-from-input" type="search" placeholder="Rechercher" aria-label="Search"/>
             <button className="nav-from-button" type="submit">Rechercher</button>
         </form>
     </nav>
-
-    
-
   );
 }
